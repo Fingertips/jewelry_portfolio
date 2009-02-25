15d9e55658ce58d288e8b5290011280cb8fc6bd1
Eloy Duran <eloy.de.enige@gmail.com>
2009-02-25 02:25:42 +0100

    Render the template with partials.

--- lib/repo_page_san.rb
+++ lib/repo_page_san.rb
@@ -2,14 +2,47 @@ require 'erb'
 
 module RepoPageSan
   class Page
-    attr_reader :template
-    
-    def initialize(template)
-      @template = template
+    class PartialBinding
+      instance_methods.each { |meth| undef_method(meth) unless meth =~ /\A__|instance_eval/ }
+      
+      def initialize(local_variables)
+        @local_variables = local_variables
+        
+        local_variables.each do |name, value|
+          instance_eval %{
+            def #{name}
+              @local_variables[#{name.inspect}]
+            end
+          }
+        end
+      end
+      
+      public :binding
     end
     
-    def render(spec)
-      ERB.new(File.read(@template)).result(binding)
+    attr_reader :template, :specs
+    
+    def initialize(template, specs)
+      @template, @specs = template, specs
+    end
+    
+    def render
+      erb @template, binding
+    end
+    
+    def partial(name, local_variables)
+      partial = File.join(template_dir, "#{name}.html.erb")
+      erb partial, PartialBinding.new(local_variables).binding
+    end
+    
+    private
+    
+    def template_dir
+      File.dirname(@template)
+    end
+    
+    def erb(file, binding)
+      ERB.new(File.read(file)).result(binding)
     end
   end
 end
--- test/repo_page_san_test.rb
+++ test/repo_page_san_test.rb
@@ -1,23 +1,42 @@
 require File.expand_path('../test_helper', __FILE__)
 
+class GemSpecMock
+  attr_accessor :name, :description
+  
+  def initialize(name, description)
+    @name, @description = name, description
+  end
+end
+
 describe "RepoPageSan::Page" do
   before do
-    @page = RepoPageSan::Page.new(fixture('template.html.erb'))
+    @specs = [
+      GemSpecMock.new('dr-nic-magic-awesome', 'Magically fix your projects overnight!'),
+      GemSpecMock.new('microgem', 'Clean room implementation of the rubygems ‘install’ command.')
+    ]
+    
+    @page = RepoPageSan::Page.new(fixture('template.html.erb'), @specs)
   end
   
   it "should return the path to the template" do
     @page.template.should == fixture('template.html.erb')
   end
   
-  it "should render a template with the given object available as `spec'" do
-    File.stubs(:read).returns('<h1><%= spec[:name] %></h1>')
-    spec = { :name => 'drnic-magic-awesome' }
-    
-    @page.render(spec).should == '<h1>drnic-magic-awesome</h1>'
+  it "should return the specs" do
+    @page.specs.should == @specs
   end
   
-  it "should render the template file with the given object" do
-    @page.render(:title => 'Dr. Nic Magic Awesome').should ==
-      File.read(fixture('expected_template_output.html'))
+  it "should render with the specified gem specs available as `specs'" do
+    File.stubs(:read).returns('<%= specs.inspect %>')
+    @page.render.should == @specs.inspect
+  end
+  
+  it "should render an ERB partial with the specified local variables" do
+    @page.partial('spec', :spec => @specs.first).should ==
+      File.read(fixture('dr-nic-magic-awesome.html'))
+  end
+  
+  it "should render the ERB template" do
+    @page.render.should == File.read(fixture('template.html'))
   end
 end
