0e1debb234359e92e59b00ffbff0bb1430040e13
Eloy Duran <eloy.de.enige@gmail.com>
2009-02-25 10:48:10 +0100

    Re-dsicovered the actual reason to use `for var in collection` instead of using #each, it doesn't create a new scope. Removed the binding class.

--- lib/repo_page_san.rb
+++ lib/repo_page_san.rb
@@ -1,37 +1,21 @@
 require 'erb'
 
 class RepoPageSan
-  class PartialBinding
-    instance_methods.each { |meth| undef_method(meth) unless meth =~ /\A__|instance_eval/ }
-    
-    def initialize(local_variables)
-      @local_variables = local_variables
-      
-      local_variables.each do |name, value|
-        instance_eval %{
-          def #{name}
-            @local_variables[#{name.inspect}]
-          end
-        }
-      end
-    end
-    
-    public :binding
-  end
-  
   attr_reader :template, :specs
   
   def initialize(template, specs)
-    @template, @specs = "#{template}.html.erb", specs
+    @template, @specs = html_template_filename(template), specs
   end
   
   def render
     erb @template, binding
   end
   
-  def partial(name, local_variables)
-    partial = File.join(view_path, "#{name}.html.erb")
-    erb partial, PartialBinding.new(local_variables).binding
+  def partial(partial_name, local_variables)
+    for (var_name, var_value) in local_variables
+      eval "#{var_name} = var_value"
+    end
+    erb html_template_file(partial_name), binding
   end
   
   def spec_partial(spec, local_variables = {})
@@ -46,5 +30,13 @@ class RepoPageSan
   
   def erb(file, binding)
     ERB.new(File.read(file)).result(binding)
+  end
+  
+  def html_template_filename(name)
+    "#{name}.html.erb"
+  end
+  
+  def html_template_file(name)
+    File.join(view_path, html_template_filename(name))
   end
 end
