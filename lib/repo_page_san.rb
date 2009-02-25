72103d860df5464d7f455ba96cdd8b07d43673a6
Eloy Duran <eloy.de.enige@gmail.com>
2009-02-25 01:42:10 +0100

    Render the template through ERB.

--- lib/repo_page_san.rb
+++ lib/repo_page_san.rb
@@ -1,9 +1,15 @@
+require 'erb'
+
 module RepoPageSan
   class Page
     attr_reader :template
     
     def initialize(template)
       @template = template
+    end
+    
+    def render(spec)
+      ERB.new(File.read(@template)).result(binding)
     end
   end
 end
--- test/repo_page_san_test.rb
+++ test/repo_page_san_test.rb
@@ -8,4 +8,16 @@ describe "RepoPageSan::Page" do
   it "should return the path to the template" do
     @page.template.should == fixture('template.html.erb')
   end
+  
+  it "should render a template with the given object available as `spec'" do
+    File.stubs(:read).returns('<h1><%= spec[:name] %></h1>')
+    spec = { :name => 'drnic-magic-awesome' }
+    
+    @page.render(spec).should == '<h1>drnic-magic-awesome</h1>'
+  end
+  
+  it "should render the template file with the given object" do
+    @page.render(:title => 'Expected Template Output').should ==
+      File.read(fixture('expected_template_output.html'))
+  end
 end
